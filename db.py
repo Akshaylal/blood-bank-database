6b7907ac2daae8ed106e11a06c9cd3e181e89adb
Akshaylal <akshaylals2000@gmail.com>
2021-01-18 20:17:48 +0530

    Database

--- db.py
+++ db.py
@@ -3,23 +3,34 @@ import mysql.connector
 DATABASE='BloodBankDB'
 
 class BBDB:
-    def __init__(self):
-        pass
+    def __init__(self, host, user, password):
+        self.connectDB(host, user, password)
+        mycursor = self.db.cursor()
     
-    def connectDB(self):
-        pass
+    def connectDB(self, host, user, password):
+        try:
+            self.db = mysql.connector.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=DATABASE
+            )
+            self.status = 'Connected'
+        except mysql.connector.Error as err:
+            print(err)
+            if err.errno == 2003:
+                self.status = 'Cannot conect to server {host}'
+            elif err.errno == 1698:
+                self.status = 'Access denied'
+            elif err.errno == 1049:
+                initDB()
+        except:
+            self.status = f'Something Went Wrong: {err}'
+        print(self.status)
     
     def initDB(self):
-        pass
+        mycursor = self.db.cursor()
+        mycursor.execute(f'CREATE DATABASE {DATABASE}')
 
 if __name__ == '__main__':
-    mydb = mysql.connector.connect(
-        host='localhost',
-        user='bloodbank',
-        password='bloodbank'
-    )
-    print(mydb)
-    mycursor = mydb.cursor()
-    mycursor.execute('SHOW DATABASES')
-    for x in mycursor:
-        print(x)
+    x=BBDB('localhost', 'bloodbank', 'bloodbank')
